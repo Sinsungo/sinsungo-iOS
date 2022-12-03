efb35e488b8ea82020e05a15867c9e626890f0a0
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-12-03 22:21:09 +0900

    dev/start

---
+++ sinsungo/sinsungo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sinsungo
+//
+//  Created by 원동진 on 2022/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sinsungo/sinsungoUITests/sinsungoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sinsungoUITestsLaunchTests.swift
+//  sinsungoUITests
+//
+//  Created by 원동진 on 2022/12/03.
+//
+
+import XCTest
+
+final class sinsungoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
